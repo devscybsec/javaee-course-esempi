e9c64c7c21694e120d96f6d7956771b2ab1b7b58
Mirko Abbrescia <mabbrescia@cybsec.it>
2019-09-23 17:57:08 +0200

    Esempio utilizzo collection di tipo List ed array / matrici

---
+++ collections-esempio/src/it/cybsec/Applicazione.java
@@ -0,0 +1,85 @@
+package it.cybsec;
+
+import java.util.*;
+
+public class Applicazione {
+
+	static int[] array;
+	static int[][] matrix; 
+	static List<String> studenti; 
+	static ArrayList<String> studentiArray;
+	static LinkedList<String> studentiLinked;
+	static Vector<String> studentiVector;
+	static Stack<String> studentiStack;
+	
+	public static void main(String[] args) {
+		
+		array = new int[10];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = 0;
+		}
+		
+		matrix = new int[10][10];
+		for (int i = 0; i < matrix.length; i++) {
+			//matrix[i] = new int[10];
+			for (int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j] = i + j;
+			}
+		}
+		System.out.println();
+		
+		studenti = new ArrayList<String>();
+		studentiArray = new ArrayList<String>();
+		studentiLinked = new LinkedList<String>();
+		studentiVector = new Vector<String>();
+		studentiStack = new Stack<String>();
+		
+		studenti.add("Mirko");
+		studenti.add("Fabiano");
+		studenti.add("Mauro");
+		studenti.add("Michele");
+		studenti.add("Tommaso");
+		studenti.add("Giuseppe");
+		
+		for (int i = 0; i < studenti.size(); i++) {
+			System.out.println(studenti.get(i));
+		}
+		System.out.println();
+		for (String el: studenti) {
+			System.out.println(el);
+		}
+		System.out.println();
+		
+		studenti.remove(0);
+		System.out.println(studenti.size());
+		studenti.remove("Michele");
+		System.out.println(studenti.size());
+		System.out.println();
+
+		Iterator<String> i = studenti.iterator();
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
+		System.out.println();
+		
+		studentiArray.addAll(studenti);
+		studentiLinked.addAll(studenti);
+		studentiVector.addAll(studenti);
+		studentiStack.addAll(studenti);
+		
+		studentiLinked.addFirst("Mirko");
+		studentiLinked.addFirst("Marco");
+		
+//		Mostrare alcune esempi con:
+//			- studentiLinked
+//			- studentiVector
+		
+		studentiStack.push("Gervaso");
+		System.out.println(studentiStack.size());
+		System.out.println(studentiStack.pop());
+		System.out.println(studentiStack.size());
+		System.out.println();
+		
+	}
+
+}
