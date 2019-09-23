5c4d3611ebe0a8a263d885a0af2183e1b785f9b3
Mirko Abbrescia <mabbrescia@cybsec.it>
2019-09-23 17:55:21 +0200

    Aggiornamenti progetto esempio cicli

--- cicli-esempio/src/Applicazione.java
+++ cicli-esempio/src/Applicazione.java
@@ -4,23 +4,29 @@ public class Applicazione {
 	public static void main(String[] args) {
 		int i = 0;
 		
-		do {
-			System.out.println(i++);
-		} while(i < 0);
+//		System.out.println(++i);
+//		i = 0;
+//		System.out.println(i++);
 		
-		i = 0;
-		while(i < 100) {
-			System.out.println(++i);
-		}
-		for(int j = 0; j < 100; j++) {
-			System.out.println(j);
-		}
+//		i = 0;
+//		do {
+//			System.out.println(i++);
+//		} while(i < 0);
 		
-		String nome = "Giovanni";
+//		i = 0;
+//		while(i < 10) {
+//			System.out.println(i++);
+//		}
+//		for(int j = 0; j < 10; j++) {
+//			System.out.println(j);
+//		}
+		
+		String nome = "Mauro";
 		switch(nome) {
 			case "Giovanni": System.out.println(nome + " è un incapace");
 				break;
 			case "Mirko": System.out.println(nome + " ha studiato Java");
+				break;
 			default: System.out.println(nome + " sta studiando Java");
 		}
 		
