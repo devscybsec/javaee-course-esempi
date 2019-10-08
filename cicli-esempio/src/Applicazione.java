28580cf91768275db55c2fb8e6a435fa757deedf
Mirko Abbrescia <mabbrescia@cybsec.it>
2019-10-08 15:51:20 +0200

    Rimozione warning variabile i

--- cicli-esempio/src/Applicazione.java
+++ cicli-esempio/src/Applicazione.java
@@ -4,9 +4,9 @@ public class Applicazione {
 	public static void main(String[] args) {
 		int i = 0;
 		
-//		System.out.println(++i);
-//		i = 0;
-//		System.out.println(i++);
+		System.out.println(++i);
+		i = 0;
+		System.out.println(i++);
 		
 //		i = 0;
 //		do {
